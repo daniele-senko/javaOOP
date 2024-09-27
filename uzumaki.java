fcd8eab2b0e4dadd78174680cf17cb23a289a507
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-09-27 03:14:54 +0000

    feat: add Uzumaki class with base attack and infinite chakra method

---
+++ uzumaki.java
@@ -0,0 +1,13 @@
+public class uzumaki extends ninja {
+
+    boolean temBiju;
+
+    public void chacraInfinito(){
+        System.out.println("Você é um Uzumaki, então você tem chara infinito ");
+    }
+
+    @Override
+    public void ataqueBase() {
+        System.out.println("Eu sou um ninja e taquei uma Kunai elemento água");
+    }
+}
