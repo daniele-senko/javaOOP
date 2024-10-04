23dd6e02746fddec9587383a16e9f6698efd1cf8
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-10-04 00:22:44 +0000

    feat: create Pessoas class with attributes and getter/setter methods

---
+++ Herança/Pessoas.java
@@ -0,0 +1,31 @@
+public class Pessoas {
+    protected String nome;
+    protected int idade;
+    protected String genero;
+    protected String telefone;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+}
