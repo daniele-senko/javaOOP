03cc7879477681001f50ad0c2e146e783bc5a9b2
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-10-04 00:53:25 +0000

    feat: add report generation for PessoaFisica and PessoaJuridica in main method

--- Herança/Main.java
+++ Herança/Main.java
@@ -1,13 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        PessoaFisica pf = new PessoaFisica();
-        PessoaJuridica pj = new PessoaJuridica();
+        PessoaFisica pf = new PessoaFisica("Maria", 34, "45645646", "3454846487");
+        PessoaJuridica pj = new PessoaJuridica("unemat", 25,"664515754","451546879456");
 
-        pf.setNome("João");
-        pj.setNome("Pedro");
+        System.out.println("------------- Relatório de pessoa física ------------- ");
+        System.out.println("Nome: " + pf.getNome());
+        System.out.println("Idade: " + pf.getIdade());
+        System.out.println("Telefone: " + pf.getTelefone());
+        System.out.println("CPF: " + pf.getCpf());
 
-        System.out.println("Nome pessoa física: " + pf.getNome());
-        System.out.println("Nome pessoa jurídica: " + pj.getNome());
+        System.out.println("------------- Relatório de pessoa juridica ------------- ");
+        System.out.println("Nome: " + pj.getNome());
+        System.out.println("Idade: " + pj.getIdade());
+        System.out.println("Telefone: " +pj.getTelefone());
+        System.out.println("CNPJ: " + pj.getCnpj());
     }
 }
