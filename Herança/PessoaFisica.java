1a7729e21465932d0cb18c77cfbc96c42f85698c
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-10-04 00:39:00 +0000

    feat: add constructor to PessoaFisica with parameters and super call

--- Herança/PessoaFisica.java
+++ Herança/PessoaFisica.java
@@ -1,6 +1,11 @@
 public class PessoaFisica extends Pessoas{
     private String cpf;
 
+    public PessoaFisica(String nome, int idade, String telefone, String cpf) {
+        super(nome, idade, telefone);
+        this.cpf = cpf;
+    }
+    
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
