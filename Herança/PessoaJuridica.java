526f61833c05486ee5000992c5835d27abb7503e
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-10-04 00:37:49 +0000

    feat: add constructor to PessoaJuridica with parameters and super call

--- Herança/PessoaJuridica.java
+++ Herança/PessoaJuridica.java
@@ -1,6 +1,11 @@
 public class PessoaJuridica extends Pessoas {
     private String cnpj;
 
+    public PessoaJuridica(String nome, int idade, String telefone, String cnpj) {
+        super(nome, idade, telefone);
+        this.cnpj = cnpj;
+    }
+    
     public void setCnpj(String cnpj) {
         this.cnpj = cnpj;
     }
