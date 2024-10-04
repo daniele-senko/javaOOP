1d81d4cc7f3b9281efb0b39e933bab45c7d62395
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-10-04 00:24:46 +0000

    feat: add PessoaFisica class extending Pessoas with CPF attribute and methods

---
+++ Herança/PessoaFisica.java
@@ -0,0 +1,11 @@
+public class PessoaFisica extends Pessoas{
+    private String cpf;
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+}
+
